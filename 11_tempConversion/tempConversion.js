7116aaa77e23ee7ffc2090c678983aa80138c5db
adamdrsinc <adam.sinc124@gmail.com>
2025-07-16 12:52:26 -0600

    Completed

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  //-32 * 0.5556
+  return Math.round(((farenheit - 32) * 0.5556) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // * 1.8 + 32
+  return Math.round(((celsius * 1.8) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
