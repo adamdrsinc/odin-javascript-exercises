2c47780aa1f4deb132e8eb96deee64f4189af147
adamdrsinc <adam.sinc124@gmail.com>
2025-07-16 12:20:11 -0600

    Completed.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR";
+    }
 
+    if(a < 0 || b < 0){
+        return "ERROR";
+    }
+
+    let num1 = Math.min(a, b);
+    let num2 = Math.max(a, b);
+    let sum = 0;
+
+    for(let i = num1; i <= num2; i++){
+        sum += i;
+    }
+
+    return sum;
+    
 };
 
 // Do not edit below this line
