35eae79f03b9c4e0f5cb3f2aaebb9f961b0365a7
adamdrsinc <adam.sinc124@gmail.com>
2025-07-16 11:25:00 -0600

    Completed

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitString = string.split('');
+    let reversed = "";
+    for(let i = splitString.length-1; i >= 0; i--){
+        reversed += splitString[i];
+    }
 
+    return reversed;
 };
 
 // Do not edit below this line
