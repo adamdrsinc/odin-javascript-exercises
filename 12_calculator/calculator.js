ffb81e413b8f430b56f716f438a8bd0ae0326090
adamdrsinc <adam.sinc124@gmail.com>
2025-07-21 14:19:27 -0600

    Finished

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, curr) => acc * curr);
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return powerHelper(num, power, 1);
 };
 
-const factorial = function() {
-	
+function powerHelper(number, power, currentIteration){
+  if(currentIteration === power - 1){
+    return number * number;
+  }
+
+  return powerHelper(number, power, currentIteration + 1) * number;
+}
+
+const factorial = function(factorial) {
+  return factorialHelper(factorial);
 };
+
+function factorialHelper(num){
+  if(num <= 1){
+    return 1;
+  }
+
+  return factorialHelper(num - 1, factorial) * num;
+}
 
 // Do not edit below this line
 module.exports = {
