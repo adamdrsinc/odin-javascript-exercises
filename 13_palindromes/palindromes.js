775b4837d7af34a0c6565785cb1401833ba4efb4
adamdrsinc <adam.sinc124@gmail.com>
2025-07-21 14:43:08 -0600

    Finished

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const normalizedWord = sanitize(word.toLowerCase());
 
+    let wordArray = normalizedWord.split(''); 
+    if(wordArray.length === 1) return true;
+
+    let lastIndex = wordArray.length - 1;
+
+    for(let i = 0; i <= Math.trunc(lastIndex / 2); i++){
+        if(wordArray[i] !== wordArray[lastIndex]){
+            return false;
+        }
+        lastIndex--;
+    }
+
+    return true;
 };
+
+function sanitize(sentence){
+    return sentence.replace(/[^a-zA-Z0-9]/g, '');
+}
 
 // Do not edit below this line
 module.exports = palindromes;
