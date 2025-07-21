e3daffa5459d8a19ea801cdc92ec3552b08e8612
adamdrsinc <adam.sinc124@gmail.com>
2025-07-21 15:17:29 -0600

    Completed

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    const newArr = [];
 
+    array.forEach(element => {
+        newArr.push(element.title);
+    });
+
+    return newArr;
 };
 
 // Do not edit below this line
